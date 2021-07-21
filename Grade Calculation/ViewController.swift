36d014f5657dd52e75668b474447cd1bf801ff02
surachet.y <aottapkub@hotmail.com>
2021-07-21 14:52:19 +0700

    Initial Commit

---
+++ Grade Calculation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Grade Calculation
+//
+//  Created by Surachet Yaitammasan on 21/7/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
